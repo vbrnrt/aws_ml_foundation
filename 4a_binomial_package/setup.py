cae5ab20107bf77d82059efa93e997f538344e3f
Lawrence <lawrence.lee@commscope.com>
2020-06-07 21:06:19 +0000

    Add file to backup exercise

--- 4a_binomial_package/setup.py
+++ 4a_binomial_package/setup.py
@@ -5,3 +5,9 @@ setup(name='distributions',
       description='Gaussian distributions',
       packages=['distributions'],
       zip_safe=False)
+
+setup(name='distributions',
+      version='0.1',
+      description='Binomial distributions',
+      packages=['distributions'],
+      zip_safe=False)
